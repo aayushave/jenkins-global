000e1811cad74e28039cba38748928ba9562ce27
aayushave <31796139+aayushave@users.noreply.github.com>
2023-09-06 14:46:20 +0530

    Update scm.groovy

--- vars/scm.groovy
+++ vars/scm.groovy
@@ -1,4 +1,4 @@
-def checkout(branch, poll, credentials, url) {
+def checkout(branch, credentials, poll, url) {
     stage('Checkout-Stage') {
         steps {
             git branch: ${branch} , changelog: false, credentialsId: ${credentials}, poll: ${poll}, url: ${url}
