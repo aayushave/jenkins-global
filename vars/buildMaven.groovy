09f09c904fb760134f86625e5be40d5d30876b12
aayushave <31796139+aayushave@users.noreply.github.com>
2023-09-09 00:22:18 +0530

    execute permissions on buildMaven.groovy

--- vars/buildMaven.groovy
+++ vars/buildMaven.groovy
@@ -2,6 +2,7 @@ def buildJarFromDirectory(sourceDir) {
             // Change the working directory to the specified source directory
             dir(sourceDir) {
                 // Replace with your build command to generate the JAR file
-                 sh "./mvnw clean install"
+                        chmod +x -R sourceDir
+                        sh "./mvnw clean install"
             }
         }
