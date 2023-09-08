68a0ee943df0143ef4a69cf0fe20e0d38dadd8cb
aayushave <31796139+aayushave@users.noreply.github.com>
2023-09-09 01:01:10 +0530

    Update buildMaven.groovy

--- vars/buildMaven.groovy
+++ vars/buildMaven.groovy
@@ -5,6 +5,7 @@ def buildJarFromDirectory(sourceDir) {
                 // Replace with your build command to generate the JAR file
                         // sh " chmod +x -R ${sourceDir}"
                         // sh "./mvnw clean install"
+                        sh "pwd"
                         sh "mvn clean install -DSkipTests"
             }
         }
