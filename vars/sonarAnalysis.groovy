50c371cd13ad0e366ad8a478619b3d73b9f932f1
aayushave <31796139+aayushave@users.noreply.github.com>
2023-09-11 18:46:57 +0530

    Create sonarAnalysis.groovy

---
+++ vars/sonarAnalysis.groovy
@@ -0,0 +1,12 @@
+def sonarAnalysis(sonarUrl,dbUrl,dbUsername,dbPassword){
+  withEnv(["PATH+SONAR_SCANNER=${scannerHome}/bin"]) {
+                        bat """
+                            mvn sonar:sonar \
+                            -Dsonar.host.url=http://your-sonarqube-server:9000 \
+                            -Dsonar.projectKey=your-project-key \
+                            -Dsonar.projectName=YourProjectName \
+                            -Dsonar.projectVersion=1.0
+                            -Dsonar.jdbc.url=${SONAR_DB_URL} \
+                            -Dsonar.jdbc.username=${SONAR_DB_USERNAME} \
+                            -Dsonar.jdbc.password=${SONAR_DB_PASSWORD}
+}
