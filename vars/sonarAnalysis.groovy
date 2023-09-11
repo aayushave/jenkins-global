3ddc739b55bcea32626904ce797d6a06ffc2fb97
aayushave <31796139+aayushave@users.noreply.github.com>
2023-09-11 19:17:49 +0530

    Update sonarAnalysis.groovy

--- vars/sonarAnalysis.groovy
+++ vars/sonarAnalysis.groovy
@@ -1,13 +1,15 @@
-def sonarWinAnalysis(sonarUrl,sonarToken,sonarProjectKey,sonarProjectName,dbUrl,dbUsername,dbPassword){
-  withEnv(["PATH+SONAR_SCANNER=${scannerHome}/bin"]) {
-                        bat """
-                            mvn sonar:sonar \
-                            -Dsonar.host.url=${sonarUrl} \
-                            -Dsonar.projectKey=${sonarProjectKey} \
-                            -Dsonar.projectName=${sonarProjectName} \
-                            -Dsonar.projectVersion=1.0
-                            -Dsonar.token=${sonarToken}
-                            -Dsonar.jdbc.url=${dbUrl} \
-                            -Dsonar.jdbc.username=${dbUsername} \
-                            -Dsonar.jdbc.password=${dbPassword}
+def sonarWinAnalysis(sourceDir,sonarUrl,sonarToken,sonarProjectKey,sonarProjectName,dbUrl,dbUsername,dbPassword){
+  // withEnv(["PATH+SONAR_SCANNER=${scannerHome}/bin"]) {
+  dir(sourceDir) {
+    bat """mvn sonar:sonar \
+           -Dsonar.host.url=${sonarUrl} \
+           -Dsonar.projectKey=${sonarProjectKey} \
+           -Dsonar.projectName=${sonarProjectName} \
+           -Dsonar.projectVersion=1.0
+           -Dsonar.token=${sonarToken}
+           -Dsonar.jdbc.url=${dbUrl} \
+           -Dsonar.jdbc.username=${dbUsername} \
+           -Dsonar.jdbc.password=${dbPassword}
+        """
+  }
 }
