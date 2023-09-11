cd5eb432956b429470d548387ac563301604d495
aayushave <31796139+aayushave@users.noreply.github.com>
2023-09-12 00:08:02 +0530

    Update owaspDependencyCheck.groovy

--- vars/owaspDependencyCheck.groovy
+++ vars/owaspDependencyCheck.groovy
@@ -1,11 +1,12 @@
 def checkDependency(sourceDir){
     // dir(sourceDir) {
-      dependencyCheck additionalArguments: ''' 
-        -o './OWASP-Data'
-        -s './OWASP-Data'
+        bat "mkdir -p ${sourceDir}"
+        dependencyCheck additionalArguments: ''' 
+        -o ${sourceDir}
+        -s ${sourceDir}
         -f 'ALL' 
         --prettyPrint''', odcInstallation: 'OWASP-Dependency-Check'
-              
+
       dependencyCheckPublisher pattern: 'dependency-check-report.xml unstableTotalAllCritical:5'
     // }
 }
