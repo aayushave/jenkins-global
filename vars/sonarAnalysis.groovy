6108206efe2485801c6e3bd07db3a3ed8de2f28b
aayushave <31796139+aayushave@users.noreply.github.com>
2023-09-11 19:13:05 +0530

    Update sonarAnalysis.groovy

--- vars/sonarAnalysis.groovy
+++ vars/sonarAnalysis.groovy
@@ -1,12 +1,13 @@
-def sonarWinAnalysis(sonarUrl,sonarToken,sonarProjectKey,dbUrl,dbUsername,dbPassword){
+def sonarWinAnalysis(sonarUrl,sonarToken,sonarProjectKey,sonarProjectName,dbUrl,dbUsername,dbPassword){
   withEnv(["PATH+SONAR_SCANNER=${scannerHome}/bin"]) {
                         bat """
                             mvn sonar:sonar \
-                            -Dsonar.host.url=http://your-sonarqube-server:9000 \
-                            -Dsonar.projectKey=your-project-key \
-                            -Dsonar.projectName=YourProjectName \
+                            -Dsonar.host.url=${sonarUrl} \
+                            -Dsonar.projectKey=${sonarProjectKey} \
+                            -Dsonar.projectName=${sonarProjectName} \
                             -Dsonar.projectVersion=1.0
-                            -Dsonar.jdbc.url=${SONAR_DB_URL} \
-                            -Dsonar.jdbc.username=${SONAR_DB_USERNAME} \
-                            -Dsonar.jdbc.password=${SONAR_DB_PASSWORD}
+                            -Dsonar.token=${sonarToken}
+                            -Dsonar.jdbc.url=${dbUrl} \
+                            -Dsonar.jdbc.username=${dbUsername} \
+                            -Dsonar.jdbc.password=${dbPassword}
 }
