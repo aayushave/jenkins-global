e3dd1449e553ef05c9db34091128262474ce7633
aayushave <31796139+aayushave@users.noreply.github.com>
2023-09-12 00:34:49 +0530

    Update owaspDependencyCheck.groovy

--- vars/owaspDependencyCheck.groovy
+++ vars/owaspDependencyCheck.groovy
@@ -1,15 +1,15 @@
 def checkDependency(sourceDir){
     // dir(sourceDir) {
-    // def windowsSourceDir = sourceDir.replaceAll('/', '\\\\')
-    // bat "echo ${windowsSourceDir}"    
-    // bat "mkdir ${windowsSourceDir}"
+    def windowsSourceDir = sourceDir.replaceAll('/', '\\\\')
+    bat "echo ${windowsSourceDir}"    
+    bat "mkdir ${windowsSourceDir}"
     dependencyCheck additionalArguments: ''' 
     -o ${sourceDir}
     -s ${sourceDir}
     -f 'ALL' 
     --prettyPrint''', odcInstallation: 'OWASP-Dependency-Check'
 
-      dependencyCheckPublisher pattern: 'dependency-check-report.xml unstableTotalAllCritical:5'
+      dependencyCheckPublisher pattern: 'dependency-check-report.xml, unstableTotalAllCritical:5'
     // }
 }
   
