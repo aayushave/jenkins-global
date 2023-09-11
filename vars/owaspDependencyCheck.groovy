f2cde52b9d7c9cad3eb0744de6f7830df78fbed2
aayushave <31796139+aayushave@users.noreply.github.com>
2023-09-12 01:08:27 +0530

    Update owaspDependencyCheck.groovy

--- vars/owaspDependencyCheck.groovy
+++ vars/owaspDependencyCheck.groovy
@@ -1,9 +1,15 @@
 def checkDependency(sourceDir){
     // dir(sourceDir) {
     def windowsSourceDir = sourceDir.replaceAll('/', '\\\\')
-    bat "echo ${windowsSourceDir}" 
-    bat "echo ${sourceDir}"
-    bat "mkdir ${windowsSourceDir}"
+    // bat "echo ${windowsSourceDir}" 
+    // bat "echo ${sourceDir}"
+    
+    def dirExists = bat(script: "if exist ${windowsSourceDir} (exit 0) else (exit 1)", returnStatus: true) == 0
+    
+    if (!dirExists) {
+        bat "mkdir ${windowsSourceDir}"
+    }
+    
     dependencyCheck additionalArguments: """ 
         -o "${sourceDir}"
         -s "${sourceDir}"
