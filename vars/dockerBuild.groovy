7bc1bd244449b3f95d0e9905144666a63e507d99
aayushave <31796139+aayushave@users.noreply.github.com>
2023-09-12 13:47:41 +0530

    Update dockerBuild.groovy

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,6 +1,7 @@
 def buildImage(sourceDir,imageName,imageTag){
   dir(sourceDir) {
     // docker build -t "${imageName}"":""${imageTag}"
-    sh "docker build -t ${imageName}:${imageTag} ."
+    // sh "docker build -t ${imageName}:${imageTag} ."
+    bat "docker build -t ${imageName}:${imageTag} ."
   }
 }
