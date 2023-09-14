4138c6d8d467d34b96823503d91ee346252db5b3
aayushave <31796139+aayushave@users.noreply.github.com>
2023-09-14 19:49:22 +0530

    Update kubectl.groovy

--- vars/kubectl.groovy
+++ vars/kubectl.groovy
@@ -1,10 +1,10 @@
-def apply(sourceDir,fileName){
+def apply(sourceDir,fileName=""){
     dir(sourceDir) {
       bat "kubectl apply -f ${sourceDir}/${fileName}"
     }
   }
 
-def delete (sourceDir,fileName){
+def delete (sourceDir,fileName=""){
       dir(sourceDir) {
       bat "kubectl delete -f ${sourceDir}/${fileName}"
     }
