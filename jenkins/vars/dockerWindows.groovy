1b75ab99349310cfc94cb9110283319c9fd36955
aayushave <31796139+aayushave@users.noreply.github.com>
2023-09-20 14:21:44 +0530

    Update dockerWindows.groovy

--- jenkins/vars/dockerWindows.groovy
+++ jenkins/vars/dockerWindows.groovy
@@ -1,13 +1,24 @@
+// Build Docker Image
+// Param:: 
+// sourceDir: dockerfile location
+// registryUrl: Nexus Repository Url
+// registryPort: Nexus Repository Port
+// imageName: Name to be given to Image
+// imageTag: Version number to be given (Ex 1.0)
 def buildImage(sourceDir,registryUrl,registryPort,imageName,imageTag){
   dir(sourceDir) {
-    // docker build -t "${imageName}"":""${imageTag}"
-    // sh "docker build -t ${imageName}:${imageTag} ."
     bat "docker build -t ${imageName}:${imageTag} ."
   }
 }
 
+// Push Docker Image
+// Param:: 
+// credentials: Jenkins Added Credential 
+// registryUrl: Nexus Repository Url
+// registryPort: Nexus Repository Port
+// imageName: Name to be given to Image
+// imageTag: Version number to be given (Ex 1.0)
 def pushImage(credentials,registryUrl,registryPort,imageName,imageTag){
-
   withCredentials([usernamePassword(credentialsId: credentials, usernameVariable: 'USER', passwordVariable: 'PASSWORD')]) {
       bat "docker login -u $USER -p $PASSWORD $registryUrl:$registryPort" 
       }
